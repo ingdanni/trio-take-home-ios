af204dcfdf42702e323fe1ef9b7479f3068d1088
Danny Narvaez <ing.danni@gmail.com>
2022-01-07 08:34:57 -0600

    Initial commit

---
+++ RestaurantMenuApp/RestaurantMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuAppApp.swift
+//  RestaurantMenuApp
+//
+//  Created by Danny Narvaez on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
